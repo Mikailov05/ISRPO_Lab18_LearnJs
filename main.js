ab7ca4fafef1416c912278448743d009bda262cd
Mikailov05 <ahmed.mikailov@interner.ru>
2026-03-12 21:53:33 +0300

    Step 8: explicit and implicit type conversion

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ console.log("Age:", age);
 console.log("Is student:", isStudent);
 */
 
-
+/*
 let newPrice;
 newPrice = 254;
 console.log("Значение:", newPrice);
@@ -127,3 +127,30 @@ newPrice = "100 рублей";
 // 5. Снова выведите значение и тип
 console.log("Значение:", newPrice);
 console.log("Тип:", typeof newPrice);
+*/
+
+
+let num = 42;
+let str = String(num);
+let str2 = num.toString();
+let str3 = "" + num;
+
+
+let strNum = "123";
+let int = Number(strNum);
+let int2 = parseInt("42.5");
+let float = parseFloat("3.14");
+let int3 = "+99";
+
+let bool1 = Boolean(1);
+let bool2 = !!1;
+let bool3 = Boolean(0);
+let bool4 = Boolean("");
+
+console.log("5" + 3);
+console.log("5" - 3);
+console.log("5" * "2");
+console.log(true + 1);
+console.log(false + 1);
+console.log(null + 1);
+console.log(undefined + 1);
