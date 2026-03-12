57edb099afd6b7541b2bb1e85719eb8a129ca2e7
Mikailov05 <ahmed.mikailov@interner.ru>
2026-03-12 21:42:01 +0300

    Step 6: variables and data types in JavaScript

---
+++ main.js
@@ -0,0 +1,116 @@
+let value = 10;
+console.log(value);
+value = "Темпър это строка";
+console.log(value);
+value = true;
+console.log(value);
+
+let userName = "Алексей";
+// шаблонная строка
+console.log(`Привет, ${userName}!`);
+
+// Number (Число)
+
+// дробное число
+let price = 99.99;
+// отрицательное число
+let temperature = -15;
+// Infinity
+let infinity = 1 / 0;
+// NaN (Not a Number)
+let notANumber = 0 / 0;
+// 0.3000000000000004 (особенность JS)
+console.log(0.1 + 0.2);
+
+// BigInt (Большие целые числа)
+
+//let bigNumber = 9007199254740991 n; // добавляем `n` в конце
+let huge = BigInt("123456789012345678901234567890");
+
+let isAlive = true;
+let isWorking = false;
+let isAdult = age >= 18; // результат сравнения
+
+// Undefined (Не определено)
+
+let x; // переменная объявлена, но не присвоено значение
+let y = undefined; // явное присваивание
+
+// Null (Пустое значение)
+
+let userData = null;
+
+let id = Symbol("id"); // создает уникальный идентификатор
+
+// Object (Объект)
+
+let person = {
+    name: "Станислав",
+    age: 30,
+    isStudent: false,
+    sayHello: function() {
+        console.log("Привет!");
+    }
+};
+
+console.log(person.name);
+
+// Array (Массив)
+
+let fruits = ["яблоко", "банан", "апельсин"];
+let numbers = [1, 2, 3, 4, 5];
+let mixed = ["текс", 42, true, null];
+
+function sum(a, b) {
+    return a + b;
+}
+
+let multiply = function(x, y) {
+    return x * y;
+};
+
+console.log(sum(5, 3));
+
+// Date (Дата)
+
+let now = new Date();
+let birthday = new Date("1995-12-17");
+
+// 6.6. Арифметические операции
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// Особенность JavaScript:
+
+console.log(10 + "5"); // "105"
+console.log("10" - 5); // 5
+
+// ### Массивы:
+const numbersArray = [1, 2, 3];
+// Можно изменять элементы:
+numbersArray[0] = 10;
+console.log(numbersArray); // [10, 2, 3]
+// Нельзя переназначить:
+// numbersArray = [5, 6, 7]; // Ошибка!
+
+// ### Объекты:
+const persons = { name: "Denis", age: 18 };
+// Можно изменять свойства:
+persons.age = 50;
+persons.city = "Volgograd";
+console.log(persons); // { name: "Denis", age: 50, city: "Volgograd" }
+// Нельзя переназначить:
+// person = { name: "Stas" }; // Ошибка!
+
+let age = 20;
+let name = "Axmed"
+let isStudent = true;
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is student:", isStudent);
